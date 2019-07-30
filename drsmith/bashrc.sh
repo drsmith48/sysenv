53686d480e84e52fd58908c2d40d1c30e3f8f461
David R. Smith <drsmith@pppl.gov>
2019-07-30 10:49:30 -0400

    mods

--- drsmith/bashrc.sh
+++ drsmith/bashrc.sh
@@ -2,6 +2,7 @@
 
 echo "loading ~/sysenv/drsmith/bashrc.sh"
 
+source /usr/local/mdsplus/setup.sh
 
 PYTHONPATH="${HOME}/Documents/analysis/w7x-study/bes-modeling"
 PYTHONPATH="${PYTHONPATH}:${HOME}/Documents/repos/fdp"
