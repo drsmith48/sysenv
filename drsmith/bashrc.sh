1d77000121a769c836c931632db6ce3eb8f65563
David R. Smith <drsmith@pppl.gov>
2021-03-16 11:32:02 -0400

    mods

--- drsmith/bashrc.sh
+++ drsmith/bashrc.sh
@@ -4,8 +4,12 @@ echo "loading ~/sysenv/drsmith/bashrc.sh"
 
 source /usr/local/mdsplus/setup.sh
 
-PYTHONPATH="${HOME}/Documents/analysis/w7x-study/bes-modeling"
-PYTHONPATH="${PYTHONPATH}:${HOME}/Documents/repos/fdp"
+# python path last to first
+PYTHONPATH="/usr/local/mdsplus/python"
+PYTHONPATH="${HOME}/Documents/Repos/fdp:${PYTHONPATH}"
+PYTHONPATH="${HOME}/Documents/Projects/W7X/repos:${PYTHONPATH}"
+PYTHONPATH="${HOME}/Documents/Projects/W7X/repos/ray-optics/src:${PYTHONPATH}"
+PYTHONPATH="${HOME}/Documents/Projects/W7X/repos/turbulence-optimization/PythonTools:${PYTHONPATH}"
 
 export PYTHONPATH
 
